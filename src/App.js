c064cc38512a8756fcecaa8434d638d88e300502
Benoit <b.mignault@gmail.com>
2022-02-22 08:12:20 -0500

    Ajout d'un tests + mecanique de couverture

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 
+function sum(a, b) {
+  return a + b;
+}
+
+module.exports = sum;
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Getting started with React testing library</h2>
       </header>
     </div>
   );
 }
-
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,25 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render } from '@testing-library/react';
 import App from './App';
+/*
+test('Verifier si la page affiche correctement', () => {
+    const root = document.createElement("div");
+    ReactDOM.render(<App />, root);
+    expect(root.querySelector("h1").textContent).toBe("TODO");
+});
+*/
+// la méthode render retourne le composant <App />
+// The i flag makes the regular expression case-insensitive
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+/*
+test('Verifier si la page affiche correctement', () => {
+    const root = document.createElement("div");
+    ReactDOM.render(<App />, root);
+    expect(root.querySelector("h1").textContent).toBe("TODO");
+});
+*/
+const sum = require('./App');
+
+test('adds 1 + 2 to equal 3', () => {
+	expect(sum(1, 2)).toBe(3);
 });
